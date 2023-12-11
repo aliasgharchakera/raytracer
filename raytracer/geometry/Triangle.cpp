867c71ec72a5b615fe1d05376fe851f96355f08f
Ali Asghar <aliasghar.chakera@gmail.com>
2023-12-11 16:05:44 +0500

    fixing merge changes

--- raytracer/geometry/Triangle.cpp
+++ raytracer/geometry/Triangle.cpp
@@ -28,12 +28,52 @@ std::string Triangle::to_string() const {
   return "Triangle";
 }
 
+// Ray intersection. Set t and sinfo as per intersection with this object.
+// FIXME: Optimize this function
 bool Triangle::hit(const Ray &ray, float &t, ShadeInfo &s) const {
-  // TODO: Implement ray-triangle intersection algorithm
-  return false;
+  
+ 	double a = v0.x - v1.x, b = v0.x - v2.x, c = ray.d.x, d = v0.x - ray.o.x; 
+	double e = v0.y - v1.y, f = v0.y - v2.y, g = ray.d.y, h = v0.y - ray.o.y;
+	double i = v0.z - v1.z, j = v0.z - v2.z, k = ray.d.z, l = v0.z - ray.o.z;
+		
+	double m = f * k - g * j, n = h * k - g * l, p = f * l - h * j;
+	double q = g * i - e * k, u = e * j - f * i;
+	
+	double inv_denom  = 1.0 / (a * m + b * q + c * u);
+	
+	double e1 = d * m - b * n - c * p;
+	double beta = e1 * inv_denom;
+	
+	if (beta < 0.0)
+	 	return (false);
+	
+	double r = e * l - h * i;
+	double e2 = a * n + d * q + c * r;
+	double gamma = e2 * inv_denom;
+	
+	if (gamma < 0.0 )
+	 	return (false);
+	
+	if (beta + gamma > 1.0)
+		return (false);
+			
+	double e3 = a * p - b * r + d * u;
+	double curr_t = e3 * inv_denom;
+	
+	if (curr_t < kEpsilon)  
+		return (false);
+					
+	t = curr_t;
+	s.hit_point = ray.o + curr_t * ray.d;
+  s.ray = ray;	
+  Vector3D normal = (v1 - v0) ^ (v2 - v0);  
+	normal.normalize();
+  s.normal = normal;
+	
+	return (true);	
 }
 
+// Get bounding box.
 BBox Triangle::getBBox() const {
-  // TODO: Implement bounding box calculation
-  return BBox();
+  return BBox(min(min(v0, v1), v2), max(max(v0, v1), v2));
 }
