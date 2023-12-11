daf42d4fc2aee49ca44fce6a7243dcfa7f308edc
Mustafasohail7 <mustufasohail7@gmail.com>
2023-12-11 18:01:43 +0500

    added automatic script .bat file

---
+++ run.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem Check if the number of arguments is correct
+if "%~1"=="" (
+    echo Usage: %0 ^<source_file.cpp^>
+    exit /b 1
+)
+
+rem Get the source file from the command-line argument
+set "source_file=%~1"
+
+rem Compile C++ code
+g++ raytracer.cpp cameras/*.cpp geometry/*.cpp materials/*.cpp samplers/*.cpp utilities/*.cpp world/*.cpp build/"%source_file%".cpp -o a.exe
+
+rem Run the compiled C++ program
+a.exe
+
+rem Run the Python script
+python convert.py
