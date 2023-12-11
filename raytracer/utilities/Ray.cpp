58813ed63bc51c31a3ea7bc5d3af222ff8d16d7e
Ali Asghar <aliasghar.chakera@gmail.com>
2023-12-11 18:38:09 +0500

    misc

--- raytracer/utilities/Ray.cpp
+++ raytracer/utilities/Ray.cpp
@@ -2,7 +2,7 @@
 
 // Constructors.
 // REVIEW: Should we set origin and dir to (0, 0, 0) and w to 1?
-Ray::Ray() : o(Point3D()), d((0.0, 0.0, 1.0)), w(1) {}
+Ray::Ray() : o(Point3D()), d(Vector3D()), w(1) {}
 
 Ray::Ray(const Point3D &origin, const Vector3D &dir) : o(origin), d(dir), w(1) {}
 
